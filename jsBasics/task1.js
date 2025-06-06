4d981d03e973f6a5cac9bde56ccc95768381f091
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-06 19:24:36 +0200

    finished_1_task_from_HW_04

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let nameFirst = 'Illia';
+let age = 28;
+let positive = true;
+let negative = false;
+let empty = null;
+let unknow = undefined;
+
+console.log(`\n My name is ${nameFirst}, I am ${age} years old`)
+console.log(`\n ${positive} \n ${negative} \n ${empty} \n ${unknow}`)
