cea21431e4920723615bfe9332fa5be25a29c085
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-06 22:43:46 +0200

    fixed_task4_3

--- jsBasics/task4_3.js
+++ jsBasics/task4_3.js
@@ -3,6 +3,7 @@
 const radius = 10;
 const height = 15;
 
-let result = ((Math.PI * (radius**2)) * height);
+let result = Math.PI * (radius**2) * height;
+let toFixedResult = result.toFixed(2);
 
-console.log(`Об'єм циліндра за формулою π * radius^2 * height дорівнює ${Number(result.toFixed(2))}`)
+console.log(`Об'єм циліндра за формулою π * radius^2 * height дорівнює ${Number(toFixedResult)}`);
