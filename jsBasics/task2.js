c9cda7bf68dda007500836d9fbba9deb88e5ae97
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-06 19:35:06 +0200

    finished_task2_from_HW_04

---
+++ jsBasics/task2.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstPerson = 'John';
+let secondPerson = 'Alex';
+
+console.log("\n Hello" + " " + firstPerson + "\n " + "Hi, how are you" + " " + secondPerson + " ?");
+console.log(`\n Hello ${firstPerson}, Hey ${secondPerson}, how are you ?`);
