6d8c7ace1c093043b3273b6d9c4d72659e8e5171
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-10 19:06:44 +0200

    added_task3_2_for_from_HW_05

--- homeworks/task3_2_for.js
+++ homeworks/task3_2_for.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const constNumber = 39;
+let multiplyFunction = '';
+
+for (let i = 1; i <= 10; i++) {
+    multiplyFunction = constNumber * i;
+    console.log(`${constNumber} x ${i} = ${multiplyFunction}`)
+}
