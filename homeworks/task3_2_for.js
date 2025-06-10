afecd81efa3c8e973f09b98ebcafee0b94640c22
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-10 19:17:14 +0200

    added_;

--- homeworks/task3_2_for.js
+++ homeworks/task3_2_for.js
@@ -5,5 +5,5 @@ let multiplyFunction = '';
 
 for (let i = 1; i <= 10; i++) {
     multiplyFunction = constNumber * i;
-    console.log(`${constNumber} x ${i} = ${multiplyFunction}`)
+    console.log(`${constNumber} x ${i} = ${multiplyFunction}`);
 }
