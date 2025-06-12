129b14fd925b4318e5c5f57184e57c9963fe8469
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-12 19:00:47 +0200

    fixed_task1

--- homeworks/HW_05/task1.js
+++ homeworks/HW_05/task1.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let averageGrade = 102;
+let averageGrade = 54;
 
-if (averageGrade <= 60) {
+if (averageGrade < 60) {
     console.log('Незадовільно');
-}   else if (averageGrade >= 60 && averageGrade <= 70) {
+}   else if (averageGrade <= 70) {
     console.log('Задовільно');
-}   else if (averageGrade >=71 && averageGrade <=80) {
+}   else if (averageGrade <=80) {
     console.log('Добре');
-}   else if (averageGrade >= 81 && averageGrade <=90) {
+}   else if (averageGrade <=90) {
     console.log('Дуже добре');
-}   else if (averageGrade >= 91 && averageGrade <= 100) {
+}   else if (averageGrade <= 100) {
     console.log('Відмінно');
 }   else (console.log('Unknow this result...'));
