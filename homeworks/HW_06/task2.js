a396dea1fc5ad39ca9a2781e9e5166dfdc44d747
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-14 20:53:27 +0200

    Added_task2_from_HW_06

--- homeworks/HW_06/task2.js
+++ homeworks/HW_06/task2.js
@@ -5,7 +5,19 @@ function verifyAge(age) {
 console.log(verifyAge(15));
 console.log(verifyAge(25));
 
+
+
 const verifyAgeFuncArrow = (age) => age >= 18;
 
 console.log(verifyAgeFuncArrow(15));
 console.log(verifyAgeFuncArrow(25));
+
+
+
+function verifyRangeAge(age) {
+    return age >= 18 && age <= 60;
+}
+
+console.log(verifyRangeAge(17));
+console.log(verifyRangeAge(61));
+console.log(verifyRangeAge(59));
