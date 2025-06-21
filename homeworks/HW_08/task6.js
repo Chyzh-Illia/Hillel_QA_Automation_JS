ce0c0813db6e289837af0b6ae19f179fd954526e
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-21 12:54:55 +0200

    fixed_task6

--- homeworks/HW_08/task6.js
+++ homeworks/HW_08/task6.js
@@ -1,8 +1,9 @@
 const numbersList = [1,10,14,2,4,5,43,34];
+const arrCopy = [...numbersList]
 
-const sortArr = numbersList.sort(function (a, b) {
+const sortArr = arrCopy.sort(function (a, b) {
     return a -b ;
 })
 
-console.log(sortArr);
 console.log(numbersList);
+console.log(arrCopy);
