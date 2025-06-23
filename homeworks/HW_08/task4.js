23ecd1175db3d29f045623d98697bd2131e5e66a
Chyzh-Illia <chyzh.illia@gmail.com>
2025-06-23 18:06:04 +0200

    fixed_task4_hw_08

--- homeworks/HW_08/task4.js
+++ homeworks/HW_08/task4.js
@@ -1,11 +1,5 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const arrDoubles = [];
-
-for (let i of arr) {
-    if (i % 2 === 0) {
-        arrDoubles.push(i);
-    }
-}
+const arrDoubles = arr.filter((num) => num % 2 === 0);
 
 console.log(arrDoubles);
