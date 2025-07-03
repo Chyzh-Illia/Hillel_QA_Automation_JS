ee54b0c7c0d6013f7f05248cc1fb7b7df9047eef
Chyzh-Illia <chyzh.illia@gmail.com>
2025-07-03 20:52:20 +0200

    fixed_task2

--- homeworks/HW_11/task2.js
+++ homeworks/HW_11/task2.js
@@ -6,8 +6,25 @@ const fetchUser1 = fetch('https://jsonplaceholder.typicode.com/users/1')
 
 Promise.all([fetchTodo1, fetchUser1])
     .then(([todoData, userData]) => {
-        console.log('GET response', todoData);
+        console.log('Todo:', todoData);
         console.log('User:', userData);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+Promise.race([
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res => res.json())
+        .then(data => ({ source: 'todo', data })),
+
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.json())
+        .then(data => ({ source: 'user', data }))
+])
+    .then(result => {
+        console.log(`Response: ${result.source}`);
+        console.log(result.data);
     })
     .catch(error => {
         console.error('Error:', error);
