68146a3c548792dc87ed9af9fdbf84a1bfa2e309
Chyzh-Illia <chyzh.illia@gmail.com>
2025-07-03 20:59:23 +0200

    fixed_task3

--- homeworks/HW_11/task3.js
+++ homeworks/HW_11/task3.js
@@ -1,31 +1,52 @@
 async function fetchData() {
-    try {
-        const response1 = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        const data1 = await response1.json();
-        const response2 = await fetch('https://jsonplaceholder.typicode.com/users/1');
-        const data2 = await response2.json();
-        console.log(Promise.all([data1, data2]));
-    }   catch (error) {
-        console.error("Error:", error);
-    }
+  try {
+    const [data1, data2] = await Promise.all([
+      fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) =>
+        res.json(),
+      ),
+      fetch("https://jsonplaceholder.typicode.com/users/1").then((res) =>
+        res.json(),
+      ),
+    ]);
+
+    console.log("Todo:", data1);
+    console.log("User:", data2);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
-fetchData2();
+fetchData();
 
+async function fetchDataPromise() {
+  try {
+    const [data1, data2] = await Promise.all([
+      fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) =>
+        res.json(),
+      ),
+      fetch("https://jsonplaceholder.typicode.com/users/1").then((res) =>
+        res.json(),
+      ),
+    ]);
 
-async function  fetchDataPromise() {
-    try {
-        const [data1, data2] = await Promise.all([
-            fetch('https://jsonplaceholder.typicode.com/todos/1').then(result => result.json()),
-            fetch('https://jsonplaceholder.typicode.com/users/1').then(result => result.json())
-        ]);
-        
-        console.log("Todo:", data1);
-        console.log("User:", data2);
-    
-    }   catch (error) {
-        console.log("Error:", error);
-    }
+    console.log("Todo:", data1);
+    console.log("User:", data2);
+
+    const fastest = await Promise.race([
+      fetch("https://jsonplaceholder.typicode.com/todos/1")
+        .then((res) => res.json())
+        .then((data) => ({ source: "todo", data })),
+
+      fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then((res) => res.json())
+        .then((data) => ({ source: "user", data })),
+    ]);
+
+    console.log(`Response: ${fastest.source}`);
+    console.log(fastest.data);
+  } catch (error) {
+    console.log("Error:", error);
+  }
 }
 
 fetchDataPromise();
